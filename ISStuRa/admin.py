988d47ab2b3a536ecf94e8dcdd2959eb2ef08c24
PwnyTail <pwnytail@c3d2.de>
2016-02-22 16:30:38 +0100

    set tables to adminpage

--- ISStuRa/admin.py
+++ ISStuRa/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Location, Typ, Time_between_replacement, Category, Inventary
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Typ)
+admin.site.register(Time_between_replacement)
+admin.site.register(Category)
+admin.site.register(Inventary)
