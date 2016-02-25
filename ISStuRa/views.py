b113dcf10d222620d8bf30472836a4b6501406e7
PwnyTail <pwnytail@c3d2.de>
2016-02-25 13:04:37 +0100

    add ISStuRa index url and view

--- ISStuRa/views.py
+++ ISStuRa/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
+from .models import Inventary
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'ISStuRa/index.html'
+    context_object_name = 'inventary_list'
+
+    def get_queryset(self):
+        return Inventary.objects.order_by('-id')
