f991e84a31f749c857c1e3fc365142b5c9d59cef
PwnyTail <pwnytail@c3d2.de>
2016-02-26 20:36:29 +0100

    update min_value, inventary_nr, date_of_purchase

--- ISStuRa/models.py
+++ ISStuRa/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from datetime import date, datetime
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 @python_2_unicode_compatible  # only if you need to support Python 2
@@ -28,17 +30,18 @@ class Category(models.Model):
 
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Inventary(models.Model):
-  typ = models.ForeignKey(Typ)
-  location = models.ForeignKey(Location)
-  tbr = models.ForeignKey(Time_between_replacement)
-  category = models.ForeignKey(Category)
-  objectname = models.CharField(max_length=255)
-  vendor_str = models.CharField(max_length=255)
-  amount = models.IntegerField(default=0)
-  date_of_purchase = models.DateField(auto_now=True)
-  single_value =    models.DecimalField(max_digits=11, decimal_places=2)
-  total_value =     models.DecimalField(max_digits=11, decimal_places=2)
-  fair_value =      models.DecimalField(max_digits=11, decimal_places=2)
+  typ       = models.ForeignKey(Typ,            verbose_name='Typ')
+  location  = models.ForeignKey(Location,       verbose_name='Ort')
+  tbr       = models.ForeignKey(Time_between_replacement, verbose_name='Nutzungsdauer')
+  category  = models.ForeignKey(Category,       verbose_name='Kategorie')
+  objectname = models.CharField(max_length=255, verbose_name='Bezeichnung')
+  vendor_str = models.CharField(max_length=255, verbose_name='Herstelleridentifikation')
+  amount    = models.IntegerField(default=1,  validators=[MinValueValidator(1)],   verbose_name='Anzahl')
+  date_of_purchase = models.DateField(default=date.today, editable=True, verbose_name='Anschaffungsdatum')
+  single_value =    models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='Einzepreis')
+  total_value =     models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='Gesamtpreis')
+  fair_value =      models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='Abschreibung')
+  inventary_nr = models.CharField(max_length=255, default='-'+str(datetime.now().year)+'-', unique=False, verbose_name='Inventar Nr.')
   def __str__ (self):
-    return str(self.id) + " - " + self.objectname
+    return self.objectname
 
