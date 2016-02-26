eeb22b6e48628891b58a33235282c231f7550bb8
PwnyTail <pwnytail@c3d2.de>
2016-02-26 20:36:57 +0100

    change  Inventary admin view

--- ISStuRa/admin.py
+++ ISStuRa/admin.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from .models import Location, Typ, Time_between_replacement, Category, Inventary
 # Register your models here.
 
+class InventaryAdmin(admin.ModelAdmin):
+    list_display = ('inventary_nr', 'objectname')
+    search_fileds = ('inventary_nr', 'objectname')
+
 admin.site.register(Location)
 admin.site.register(Typ)
 admin.site.register(Time_between_replacement)
 admin.site.register(Category)
-admin.site.register(Inventary)
+admin.site.register(Inventary, InventaryAdmin)
